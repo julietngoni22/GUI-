baf65d9a0d73d5f90a0a388e323f122112d5b480
Juliet Ngoni <118878439+julietngoni22@users.noreply.github.com>
2024-07-23 08:44:51 +0200

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package secure.all.co.za;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Geeks_PC54
+ */
+public class Frame extends JFrame{
+    public Frame(){
+        setTitle("First GUI");
+        setSize(200, 250);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.red);
+        setVisible(true);
+    }
+
+    
+    
+}
