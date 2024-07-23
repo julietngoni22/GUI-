b3c06030c93185e2c59257f678ce0d18e73b5720
Juliet Ngoni <118878439+julietngoni22@users.noreply.github.com>
2024-07-23 08:37:00 +0200

    Add files via upload

---
+++ TestFrame.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testframe;
+
+import secure.all.co.za.Frame;
+
+/**
+ *
+ * @author Geeks_PC54
+ */
+public class TestFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Frame fr;
+       fr = new Frame();
+    }
+    
+}
